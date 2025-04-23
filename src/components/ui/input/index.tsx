1c779a2b9293faa55f9b01a3b7cd072c0da02860
Florian Bär <florianbar01@gmail.com>
2025-04-23 11:05:20 +0200

    create input component

---
+++ src/components/ui/input/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+interface SelectProps {
+  type?: "text" | "email" | "password";
+  initialValue?: string;
+  placeholder?: string;
+  onChange: (value: string) => void;
+}
+
+export default function Select(props: SelectProps) {
+  const { type = "text", initialValue = "", placeholder, onChange } = props;
+
+  const [value, setValue] = useState<string>(initialValue);
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    setValue(value);
+    onChange(value);
+  }
+
+  return (
+    <input
+      type={type}
+      className="border border-gray-200 rounded-md p-2.5 w-full text-[15px] font-medium"
+      value={value}
+      onChange={handleChange}
+      placeholder={placeholder}
+    />
+  );
+}
