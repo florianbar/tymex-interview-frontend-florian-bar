1cd478513aae37b52aeeb2a25a2e4183aae80bf2
Florian Bär <florianbar01@gmail.com>
2025-04-23 10:56:17 +0200

    create select component

---
+++ src/components/ui/select/index.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+interface SelectProps {
+  options: Option[];
+  value?: string;
+  placeholder?: string;
+  onChange: (value: string) => void;
+}
+
+export default function Select(props: SelectProps) {
+  const { options, value = "", placeholder, onChange } = props;
+
+  const [selectedOption, setSelectedOption] = useState<string | null>(value);
+
+  function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const value = event.target.value;
+    setSelectedOption(value);
+    onChange(value);
+  }
+
+  return (
+    <select
+      className="border border-gray-200 rounded-md px-1.5 py-2.5 w-full text-[15px] font-medium"
+      onChange={handleChange}
+      value={selectedOption || ""}
+    >
+      {placeholder && (
+        <option value="" disabled>
+          {placeholder}
+        </option>
+      )}
+
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+}
