893c0d49b12a4b6bbfadabc754a28b445264eb3b
Florian Bär <florianbar01@gmail.com>
2025-04-23 11:05:04 +0200

    simulate delay

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -10,6 +10,9 @@ export async function GET(request: Request): Promise<NextResponse> {
   const page = searchParams.get("page");
   const limit = searchParams.get("limit");
 
+  // Simulate a delay of 1 second
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const users = await getUsers(id, name, email, page, limit);
   return NextResponse.json(users);
 }
